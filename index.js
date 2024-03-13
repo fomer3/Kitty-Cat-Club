83cc7dd67bddcc533049f8332c3ccca3eabdab9b
fomer3 <123983699+fomer3@users.noreply.github.com>
2024-03-13 12:12:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import path from "path";
 import ejs from "ejs";
 import createBareServer from "@tomphttp/bare-server-node";
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3003;
 const app = express();
 const __dirname = process.cwd();
 const server = http.createServer();
@@ -82,5 +82,5 @@ server.on("listening", () => {
 });
 
 server.listen({
-  port: 3000,
+  port: 3003,
 });
